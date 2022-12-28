6a18a6bea4f2ffa9c3dd67bdfa6dd12b59326bff
tracer-nju <tracer@nju.edu.cn>
2022-12-28 21:45:53 +0800

    Linux LAPTOP-79LGJ73N 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:45:53 up  2:00,  0 users,  load average: 0.54, 0.31, 0.17

--- demo/L0/os-workbench-2022/amgame/src/keyboard.c
+++ demo/L0/os-workbench-2022/amgame/src/keyboard.c
@@ -23,7 +23,7 @@ void print_key() {
       draw_tile(rx * SIDE, ry * SIDE, SIDE, SIDE, 0xff0000); // red
     }
     switch (event.keycode) {
-      case 0x1f :
+      case 0x1 :
         if(ry * SIDE <= h)
           ry += 1;
         draw_tile(rx * SIDE, ry * SIDE, SIDE, SIDE, 0x000000); // black
