9e77b3656b05badc4f34e2a21646ea91fe0cfb90
caoboxi <541190157@qq.com>
2023-01-02 21:10:14 +0800

    add L1

--- demo/L0/os-workbench-2022/abstract-machine/klib/src/stdio.c
+++ demo/L0/os-workbench-2022/abstract-machine/klib/src/stdio.c
@@ -7,23 +7,28 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  // puts("printf Not implemented");
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // puts("vsprintf Not implemented");
+  return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // puts("sprintf Not implemented");
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  // puts("snprintf Not implemented");
+  return 0;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // puts("vsnprintf Not implemented");
+  return 0;
 }
 
 #endif
---
+++ demo/L1/os-workbench-2022/demo/build.sh
@@ -0,0 +1,5 @@
+rm a.out
+# gcc -O1 -c sum.c -lpthread && objdump -d sum.o
+# gcc -O1 sum.c -lpthread && objdump -d a.out
+gcc peterson-barrier.c -lpthread && ./a.out
+
