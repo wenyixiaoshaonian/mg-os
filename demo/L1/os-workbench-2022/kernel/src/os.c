e82055549a45eb35580f6c49153a3d8dd9a7faa0
tracer-nju <tracer@nju.edu.cn>
2023-01-08 21:24:50 +0800

    Linux LAPTOP-79LGJ73N 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:24:50 up 12:12,  0 users,  load average: 0.30, 0.15, 0.06

--- demo/L1/os-workbench-2022/kernel/src/os.c
+++ demo/L1/os-workbench-2022/kernel/src/os.c
@@ -1,5 +1,6 @@
 #include <common.h>
 
+extern spinlock_t *lock;
 static void os_init() {
   pmm->init();
 }
@@ -8,6 +9,9 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
+  spin_lock(lock);				//获取锁
+  printf(">>>===111 into spin...\n");
+  spin_unlock(lock);  			//释放锁
   while (1) ;
 }
 
