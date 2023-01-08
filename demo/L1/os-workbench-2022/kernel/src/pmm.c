1982d9cd8aeca10fa43442b0b6e99e2874263896
tracer-nju <tracer@nju.edu.cn>
2023-01-08 21:18:34 +0800

    Linux LAPTOP-79LGJ73N 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:18:34 up 12:05,  0 users,  load average: 0.18, 0.08, 0.02

--- demo/L1/os-workbench-2022/kernel/src/pmm.c
+++ demo/L1/os-workbench-2022/kernel/src/pmm.c
@@ -23,9 +23,9 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  spin_lock(lock);				//获取锁
-  printf(">>>===111 into spin...\n");
-  spin_lock(lock);  			//释放锁
+  // spin_lock(lock);				//获取锁
+  // printf(">>>===111 into spin...\n");
+  // spin_lock(lock);  			//释放锁
 }
 
 MODULE_DEF(pmm) = {
