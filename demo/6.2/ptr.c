7a7e63e830b528abd106165d32874f5d4fc9f360
caoboxi <541190157@qq.com>
2023-01-22 22:22:03 +0800

    add 6.2

--- demo/6.2/ptr.c
+++ demo/6.2/ptr.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+char big[1 << 30];
 int main() {
   unsigned *p;
   p = (void *)main;
   printf("%x \n",*p);
-  *p = 1;
+  // *p = 1;
+  while(1) {
+    sleep(1000);
+  }
   return 0;
 }
