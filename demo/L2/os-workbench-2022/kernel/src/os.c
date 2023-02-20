c0726692378f979a3c25d11a6e81440577c2cf2c
caoboxi <541190157@qq.com>
2023-02-20 09:23:53 +0800

    set base envir

--- demo/L2/os-workbench-2022/kernel/src/os.c
+++ demo/L2/os-workbench-2022/kernel/src/os.c
@@ -1,17 +1,81 @@
 #include <common.h>
 
+#define MAXBLOCK 16384   //16*1024
+
+extern spinlock_t *slock;
+enum ops { OP_ALLOC = 0, OP_FREE };
+struct malloc_op {
+  enum ops type;
+  size_t sz; 
+  void *addr; 
+   ;
+};
+
+void random_op(struct malloc_op* op) {
+  // op->type = rand() % 2;
+  op->sz = rand() % MAXBLOCK;
+  op->addr = NULL;
+  return ;
+}
+
+int alloc_check(void *adr,size_t sz) {
+  if(!adr)
+    return false;
+  size_t size = adr - 40;
+  return (adr > heap.start && adr < heap.end && size == sz) ? true:false;
+}
+
+static void stress_test() {
+  struct malloc_op op;
+  op.type = OP_ALLOC;
+  
+  while (1) {
+    random_op(&op);
+    switch (op.type) {
+      case OP_ALLOC: 
+        op.addr = pmm->alloc(op.sz);
+        if(!op.addr && !alloc_check(op.addr, op.sz)) {
+          printf("malloc adr is illegality!!!!!!\n");
+          return;
+        }
+      // printf("%d malloc adr successful  adr = %p  size = %d\n",cpu_current(),op.addr,op.sz);
+      // break;
+      // case OP_FREE:
+        kfree(op.addr); 
+
+      break;
+    }
+  }
+}
+
+// extern spinlock_t *slock;
 static void os_init() {
   pmm->init();
 }
 
 static void os_run() {
-  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+  void *test = NULL;
+  void *test2 = NULL;
+  for (const char *s = "Hello World from CPU #*\n\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  while (1) ;
+  stress_test();
+  while (1) {
+    ;
+  }
+}
+
+static Context os_trap(Event ev, Context *context) {
+
+}
+
+static void os_on_irq(int seq, int event, handler_t handler) {
+
 }
 
 MODULE_DEF(os) = {
   .init = os_init,
   .run  = os_run,
+  .trap = os_trap,
+  .on_irq = os_on_irq,
 };
--- demo/L2/os-workbench-2022/kernel/src/pmm.c
+++ demo/L2/os-workbench-2022/kernel/src/pmm.c
@@ -1,15 +1,142 @@
 #include <common.h>
 
-static void *kalloc(size_t size) {
+spinlock_t *slock;
+size_t len = 0;
+size_t used_len = 0;
+
+typedef struct x_block h_block;
+
+struct x_block {  // 40 bite
+  size_t size;    // 8 bite
+  int status;     // 4 bite
+  void *adr;      // 8 bite
+  h_block *next;  // 8 bit
+  int use_flag;    // 4 bit
+};
+
+h_block *head = NULL;
+h_block *pre = NULL;
+#define HEADSIZE sizeof(h_block)
+#define USE 1
+#define FREE 0
+
+void spin_init(spinlock_t *lk) {
+  *lk = 0;
+}
+void spin_lock(spinlock_t *lk) {
+  while (1) {
+    intptr_t value = atomic_xchg(lk, 1);
+    if (value == 0) {
+      break;
+    }
+  }
+}
+void spin_unlock(spinlock_t *lk) {
+  atomic_xchg(lk, 0);
+}
+
+h_block* find_block(size_t size) {
+  h_block* heap_block = NULL;
+  if(!pre) {
+    return NULL;
+  }
+  heap_block = head;
+  spin_lock(slock);
+  while(heap_block) {
+    if(heap_block->size == size && heap_block->status == FREE) {
+      // printf("find the memory at %p...\n",heap_block);
+      heap_block->status = USE;
+      spin_unlock(slock);
+      return heap_block;
+    }
+    // printf("memory at %p...status = %d\n",heap_block,heap_block->status);
+    heap_block = heap_block->next;
+  }
+  spin_unlock(slock);
+  // printf("can not find the memory !!!\n");
   return NULL;
 }
 
-static void kfree(void *ptr) {
+h_block* create_block(size_t size) {
+  h_block* heap_block = NULL;
+
+  spin_lock(slock);
+  if(used_len + HEADSIZE + size < len) {
+    heap_block = heap.start + used_len;
+    used_len += HEADSIZE + size;
+    printf(">>>=== %d  free size = %d \n",cpu_current(),len - used_len);
+  }
+  else {
+    printf("there is not enough memory...\n");
+    spin_unlock(slock);
+    return NULL;
+  }
+  heap_block->size = size;
+  heap_block->status = USE;
+  heap_block->adr = (int)heap_block + HEADSIZE;
+  heap_block->next = NULL;
+  heap_block->use_flag = 0x55aa;   // this block is used
+  if(pre)
+    pre->next = heap_block;
+  pre = heap_block;
+  // printf(">>== %d   %p  block->use_flag %d\n",cpu_current(),heap_block->adr,heap_block->use_flag);
+  spin_unlock(slock);
+  return heap_block;
+}
+
+bool free_block(h_block* block) {
+  spin_lock(slock);
+  if(block->status == FREE || block->use_flag != 0x55aa) {
+    // printf(">>== %d  kfree failed.....%p  block->status = %d  block->use_flag %d\n",cpu_current(),block->adr,block->status,block->use_flag);
+    spin_unlock(slock);
+    return false;
+  }
+  block->status = FREE;
+  spin_unlock(slock);
+  return true;
+}
+
+h_block* break_block() {
+  //todo
+  return NULL;
+}
+
+void *kalloc(size_t size) {
+  h_block* heap_block = find_block(size);
+  if (heap_block) {
+    return heap_block->adr;
+  }
+  else {
+    heap_block = create_block(size);
+    if (heap_block) {
+      return heap_block->adr;
+    }
+  }
+  printf("kalloc failed.....\n");
+  return NULL;
+}
+
+void kfree(void *ptr) {
+  
+  if(!ptr) {
+    return;
+  }
+  h_block* block = NULL;
+  block = (h_block*)(ptr - HEADSIZE);   //根据adr找到block头部
+  if(!free_block(block)) {
+    return;
+  }
+    // printf("kfree failed.....%p \n",ptr);
+  
+  return;
 }
 
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  spin_init(slock);
+  head = heap.start;
+  len = heap.end - heap.start;
 }
 
 MODULE_DEF(pmm) = {
