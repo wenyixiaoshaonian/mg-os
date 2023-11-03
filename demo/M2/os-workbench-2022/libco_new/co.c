7169ce2e9a7cf75b95d724b6670f0ab5e82d36da
cao boxi <541190157@qq.com>
2023-11-03 07:04:58 +0000

    初步完成

--- demo/M2/os-workbench-2022/libco_new/co.c
+++ demo/M2/os-workbench-2022/libco_new/co.c
@@ -32,10 +32,6 @@ typedef struct {
     void *ebp;
     void *eip;
     void *ebx;
-    void *r12d;
-    void *r13d;
-    void *r14d;
-    void *r15d;
 #endif
 }reg_info;
 
@@ -52,7 +48,7 @@ struct co {
 
 struct co *co_head = NULL;
 struct co *co_cur = NULL;
-reg_info *cur_reg = NULL;
+// reg_info *cur_reg = NULL;
 
 struct co * random_select() {
   int i=1;
@@ -68,6 +64,84 @@ struct co * random_select() {
   return ret;
 }
 
+void _save(reg_info *cur_ctx)
+{
+#ifdef __x86_64__
+    __asm__ __volatile__ (
+    "       movq %rsp, 0(%rdi)          \n"    // save stack pointer
+    "       movq %rbp, 8(%rdi)          \n"    // save frame pointer
+    "       movq (%rsp), %rax           \n"
+    "       movq %rax, 16(%rdi)         \n"    // save pc pointer
+    "       movq %rbx, 24(%rdi)         \n"    // save rbx, r12 - r15
+    "       movq %r12, 32(%rdi)         \n"
+    "       movq %r13, 40(%rdi)         \n"
+    "       movq %r14, 48(%rdi)         \n"
+    "       movq %r15, 56(%rdi)         \n"
+    );
+  #elif __i386__
+    __asm__ __volatile__ (
+    "       movl %esp, 0(%esi)          \n"    // save stack pointer
+    "       movl %ebp, 4(%esi)          \n"    // save frame pointer
+    "       movl (%esp), %eax           \n"
+    "       movl %eax, 8(%esi)         \n"    // save pc pointer
+    "       movl %ebx, 12(%esi)         \n"    // save rbx, r12 - r15
+    );
+  #endif
+}
+/*
+ * %rdi: cur_ctx, %rsi: new_ctx
+ * save current context to parameter_1: cur_ctx, switch context to parameter_2: new_ctx
+ */
+void _switch(reg_info *cur_ctx, reg_info *new_ctx)
+{
+#ifdef __x86_64__
+    __asm__ __volatile__ (
+    "       movq %rsp, 0(%rdi)          \n"    // save stack pointer
+    "       movq %rbp, 8(%rdi)          \n"    // save frame pointer
+    "       movq (%rsp), %rax           \n"
+    "       movq %rax, 16(%rdi)         \n"    // save pc pointer
+    "       movq %rbx, 24(%rdi)         \n"    // save rbx, r12 - r15
+    "       movq %r12, 32(%rdi)         \n"
+    "       movq %r13, 40(%rdi)         \n"
+    "       movq %r14, 48(%rdi)         \n"
+    "       movq %r15, 56(%rdi)         \n"
+    "       movq 56(%rsi), %r15         \n"
+    "       movq 48(%rsi), %r14         \n"
+    "       movq 40(%rsi), %r13         \n"    // restore rbx, r12 - r15
+    "       movq 32(%rsi), %r12         \n"
+    "       movq 24(%rsi), %rbx         \n"
+    "       movq 8(%rsi), %rbp          \n"    // restore frame pointer 
+    "       movq 0(%rsi), %rsp          \n"    // restore stack pointer
+    "       movq 16(%rsi), %rax         \n"    // restore pc pointer
+    "       movq %rax, (%rsp)           \n"    // push pc pointer in stack
+    "       ret                           "
+    );
+#elif __i386__
+    __asm__ __volatile__ (
+    "       movl %esp, 0(%esi)          \n"    // save stack pointer
+    "       movl %ebp, 4(%esi)          \n"    // save frame pointer
+    "       movl (%esp), %eax           \n"
+    "       movl %eax, 8(%esi)         \n"    // save pc pointer
+    "       movl %ebx, 12(%esi)         \n"    // save rbx, r12 - r15
+    "       movl 12(%edx), %ebx         \n"
+    "       movl 4(%edx), %ebp          \n"    // restore frame pointer 
+    "       movl 0(%edx), %esp          \n"    // restore stack pointer
+    "       movl 8(%edx), %eax         \n"    // restore pc pointer
+    "       movl %eax, (%esp)           \n"    // push pc pointer in stack
+    "       ret                           "
+    );
+#endif
+}
+
+void _exec() {
+  struct co_list *cur = NULL;
+  co_cur->func(co_cur->arg);
+  co_cur->status = CO_DEAD;
+  debug(">>>=== CO_DEAD...... co_cur = %p\n",co_cur);
+  //目前一个协程运行完成后，默认切回主程序
+  _switch(co_cur->reg,co_head->reg);
+}
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   //第一个node必然是当前main
   if(!co_head) {
@@ -76,8 +150,6 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     main_node->reg = (reg_info*)malloc(sizeof(reg_info));
 
     main_node->name = "main";
-    // main_node->func = func;
-    // main_node->arg = arg;
     main_node->status = CO_RUNNING;
     main_node->last = NULL;
     main_node->next = NULL;
@@ -98,14 +170,26 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   co_cur->next = ret;
   co_cur = ret;
 
+  //设置协程的初始状态
+#ifdef __x86_64__  
+  ret->reg->rbp = ret->stack;
+  ret->reg->rsp = ret->stack + STACK_SIZE - (sizeof(void *)*2);
+
+  ret->reg->rip = (void *)_exec;
+#elif __i386__
+  ret->reg->ebp = ret->stack;
+  ret->reg->esp = ret->stack + STACK_SIZE - (sizeof(void *)*2);
+
+  ret->reg->eip = (void *)_exec;
+#endif
+
   return ret;
 }
 
 void co_wait(struct co *co) {
-    //save(cur_reg);
   while(co->status != CO_DEAD) {
     //切换寄存器，保存运行状态到当前协程，将要运行的协程状态写入寄存器
-    // yield(cur->reg,next->reg);
+    _switch(co_cur->reg,co->reg);
   }
   //remove list
   if(!co->last) {
@@ -128,10 +212,12 @@ void co_wait(struct co *co) {
 void co_yield() {
   //随机选出下一个要运行的协程
   struct co *next = random_select();
+  if(next) {
+    //切换寄存器，保存运行状态到当前协程，将要运行的协程状态写入寄存器
+    _switch(co_cur->reg,next->reg);
+  }
+  else {
+    debug("no co can run.....\n");
+  }
 
-  //保存当前协程运行状态，将当前寄存器拷贝至内存
-  // save(cur_reg);
-
-  //切换寄存器，保存运行状态到当前协程，将要运行的协程状态写入寄存器
-  // yield(cur->reg,next->reg);
 }
